167d442fd3e15910bfd5468c1e808d5aa1d836b0
Shehroz Ali <sshehroza13@gmail.com>
2022-06-30 17:01:39 +0500

    first push

---
+++ src/main/kotlin/com/example/photobackend/PhotoBackendApplication.kt
@@ -0,0 +1,17 @@
+package com.example.photobackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class PhotoBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<PhotoBackendApplication>(*args)
+}
+
+@RestController
+class HelloController {
+	fun hello() = "Hello! I am from Spring Boot Kotlin!"
+}
