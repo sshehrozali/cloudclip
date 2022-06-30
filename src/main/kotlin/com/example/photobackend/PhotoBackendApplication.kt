fd2b1479b5fc5b9aa7884046deba517858095e93
Shehroz Ali <sshehroza13@gmail.com>
2022-06-30 17:28:02 +0500

    Created Entity for Cloud Datastore Created Repository interface for Photo entity

--- src/main/kotlin/com/example/photobackend/PhotoBackendApplication.kt
+++ src/main/kotlin/com/example/photobackend/PhotoBackendApplication.kt
@@ -1,7 +1,11 @@
 package com.example.photobackend
 
+import com.google.cloud.spring.data.datastore.core.mapping.Entity
+import com.google.cloud.spring.data.datastore.repository.DatastoreRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.stereotype.Repository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,12 +15,16 @@ class PhotoBackendApplication
 fun main(args: Array<String>) {
 	runApplication<PhotoBackendApplication>(*args)
 }
-
+@Entity
 data class Photo(
+	@Id
 	var id: String? = null,
 	var uri: String? = null,
 	var label: String? = null
 )
+
+@Repository
+interface PhotoRepository : DatastoreRepository<Photo, String>
 
 @RestController
 class HelloController {
