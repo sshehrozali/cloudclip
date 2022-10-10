b7415fec15bd1fbd85e8a2485340bb6c1956854a
Shehroz Ali <sshehroza13@gmail.com>
2022-10-10 15:35:46 +0500

    PhotoService: Implemented uploadPhoto(file) saving Image in Storage Bucket and DataStore by consuming Vision API

--- src/main/kotlin/com/photo2bucket/PhotoService.kt
+++ src/main/kotlin/com/photo2bucket/PhotoService.kt
@@ -1,10 +1,14 @@
 package com.photo2bucket
 
 import com.google.cloud.spring.vision.CloudVisionTemplate
+import com.google.cloud.vision.v1.Feature
 import org.springframework.core.io.Resource
+import org.springframework.core.io.WritableResource
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import java.util.*
 
 @Service
 class PhotoService (
@@ -23,5 +27,26 @@ class PhotoService (
         } else {
             ResponseEntity(HttpStatus.NOT_FOUND)
         }
+    }
+
+    // Method to upload a Photo to Storage Bucket
+    fun uploadPhoto(file: MultipartFile) : Photo {
+        val id = UUID.randomUUID().toString()    // Randomly generate Filename for photo
+        val uri = "$bucket/$id"        // URI to access photo from bucket
+        val gcs = ctx.getResource(uri) as WritableResource        // Write as writeable resource
+
+        // Upload file to Cloud Bucket
+        file.inputStream.use { input ->
+            gcs.outputStream.use { output ->
+                input.copyTo(output)
+            }
+        }
+        // Analyze image using Cloud Vision API
+        val response = visionTemplate.analyzeImage(file.resource, Feature.Type.LABEL_DETECTION)
+        // Get image labels
+        val labels = response.labelAnnotationsList.take(5).map { it.description }.joinToString { "," }
+
+        // Save in DB
+        return photoRepository.save(Photo(id = id, uri = uri, label = labels))
     }
 }
