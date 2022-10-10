14d5989afa4835145e743b435ebba8ab975fd1c3
Shehroz Ali <sshehroza13@gmail.com>
2022-10-10 15:05:22 +0500

    Added @Entity, @AllArgsContructor, @NoArgsContructor

--- src/main/kotlin/com/photo2bucket/Photo.kt
+++ src/main/kotlin/com/photo2bucket/Photo.kt
@@ -1,4 +1,16 @@
 package com.photo2bucket
 
+import com.google.cloud.spring.data.datastore.core.mapping.Entity
+import lombok.AllArgsConstructor
+import lombok.NoArgsConstructor
+import org.springframework.data.annotation.Id
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 class Photo {
+    @Id
+    var id: String? = null
+    var uri: String? = null
+    var label: String? = null
 }
