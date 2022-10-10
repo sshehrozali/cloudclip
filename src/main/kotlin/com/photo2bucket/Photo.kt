965d225bc74b2c27484356c766aae82f78b61342
Shehroz Ali <sshehroza13@gmail.com>
2022-10-10 15:23:10 +0500

    Photo: Added constructor paramter

--- src/main/kotlin/com/photo2bucket/Photo.kt
+++ src/main/kotlin/com/photo2bucket/Photo.kt
@@ -2,15 +2,16 @@ package com.photo2bucket
 
 import com.google.cloud.spring.data.datastore.core.mapping.Entity
 import lombok.AllArgsConstructor
+import lombok.Data
 import lombok.NoArgsConstructor
 import org.springframework.data.annotation.Id
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-class Photo {
+class Photo(id: String, uri: String, label: String) {
     @Id
-    var id: String? = null
-    var uri: String? = null
-    var label: String? = null
+    lateinit var id: String
+    lateinit var uri: String
+    lateinit var label: String
 }
