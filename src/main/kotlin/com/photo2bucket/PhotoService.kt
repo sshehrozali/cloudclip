65cd81d769f025e4a281cc04579614bdc436c866
Shehroz Ali <sshehroza13@gmail.com>
2022-10-10 15:25:20 +0500

    PhotoService: Added @Service annotation PhotoService: Cut pasted bucket path from PhotoController

--- src/main/kotlin/com/photo2bucket/PhotoService.kt
+++ src/main/kotlin/com/photo2bucket/PhotoService.kt
@@ -1,4 +1,15 @@
 package com.photo2bucket
 
-class PhotoService {
+import com.google.cloud.spring.vision.CloudVisionTemplate
+import org.springframework.stereotype.Service
+
+@Service
+class PhotoService (
+    private val photoRepository: PhotoRepository,
+    private val ctx: org.springframework.context.ApplicationContext,
+    private val visionTemplate: CloudVisionTemplate
+        ) {
+    // Bucket name
+    private val bucket = "gs://magical-photos-bucket"
+
 }
