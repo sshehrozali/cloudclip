a70ff0c29044a6748814dd26d6cba01eab2dc7e1
Shehroz Ali <sshehroza13@gmail.com>
2022-10-10 15:08:34 +0500

    PhotoRepository: Implemented interface with DataStoreRepository<Photo, String>

--- src/main/kotlin/com/photo2bucket/PhotoRepository.kt
+++ src/main/kotlin/com/photo2bucket/PhotoRepository.kt
@@ -1,4 +1,7 @@
 package com.photo2bucket
 
-interface PhotoRepository {
-}
+import com.google.cloud.spring.data.datastore.repository.DatastoreRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
+
+@RepositoryRestResource
+interface PhotoRepository : DatastoreRepository<Photo, String>
