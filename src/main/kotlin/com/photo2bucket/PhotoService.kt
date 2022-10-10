ebff726f90470eff09336a3c49b1cf9ad145c5de
Shehroz Ali <sshehroza13@gmail.com>
2022-10-10 15:27:58 +0500

    PhotoService: Implemented retrievePhoto(id: String) method to return Photo

--- src/main/kotlin/com/photo2bucket/PhotoService.kt
+++ src/main/kotlin/com/photo2bucket/PhotoService.kt
@@ -1,6 +1,9 @@
 package com.photo2bucket
 
 import com.google.cloud.spring.vision.CloudVisionTemplate
+import org.springframework.core.io.Resource
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
@@ -12,4 +15,13 @@ class PhotoService (
     // Bucket name
     private val bucket = "gs://magical-photos-bucket"
 
+    // Method to retrieve Photo from Storage Bucket
+    fun retrievePhoto(id: String) : ResponseEntity<Resource> {
+        val resource = ctx.getResource("/$bucket/$id")
+        return if (resource.exists()) {
+            ResponseEntity.ok(resource)
+        } else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
 }
