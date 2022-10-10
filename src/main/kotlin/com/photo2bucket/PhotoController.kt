c88e5ab23c7014a38d7e9f124676f5df17dfd438
Shehroz Ali <sshehroza13@gmail.com>
2022-10-10 15:19:55 +0500

    PhotoController: Formatted code

--- src/main/kotlin/com/photo2bucket/PhotoController.kt
+++ src/main/kotlin/com/photo2bucket/PhotoController.kt
@@ -12,17 +12,17 @@ import java.util.*
 
 @RestController
 @RequestMapping("/api/v1")
-class PhotoController (
+class PhotoController(
     private val photoRepository: PhotoRepository,
     private val ctx: org.springframework.context.ApplicationContext,
     private val visionTemplate: CloudVisionTemplate
-        ) {
+) {
     // Bucket name
     private val bucket = "gs://magical-photos-bucket"
 
     // Method to retrieve a Photo
     @GetMapping("/image/{id}")
-    fun get(@PathVariable id: String) : ResponseEntity<Resource> {
+    fun get(@PathVariable id: String): ResponseEntity<Resource> {
         val resource = ctx.getResource("/$bucket/$id")
         return if (resource.exists()) {
             ResponseEntity.ok(resource)
@@ -33,16 +33,16 @@ class PhotoController (
 
     // Method to upload new Photo
     @PostMapping("/upload")
-    fun upload(@RequestParam("file") file: MultipartFile) : Photo {
-        val id = UUID.randomUUID().toString()	// Randomly generate Filename for photo
-        val uri = "$bucket/$id"		// URI to access photo from bucket
+    fun upload(@RequestParam("file") file: MultipartFile): Photo {
+        val id = UUID.randomUUID().toString()    // Randomly generate Filename for photo
+        val uri = "$bucket/$id"        // URI to access photo from bucket
         val gcs = ctx.getResource(uri) as WritableResource        // Write as writeable resource
 
         // Upload file to Cloud Bucket
-        file.inputStream.use {
-                input -> gcs.outputStream.use { output ->
-            input.copyTo(output)
-        }
+        file.inputStream.use { input ->
+            gcs.outputStream.use { output ->
+                input.copyTo(output)
+            }
         }
         // Analyze image using Cloud Vision API
         val response = visionTemplate.analyzeImage(file.resource, Feature.Type.LABEL_DETECTION)
